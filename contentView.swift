20ce8bb84b73aaf568882612a072f63e3bd5fcfe
dwbacon <105027654+dwbacon@users.noreply.github.com>
2025-07-23 23:32:38 -0400

    Create contentView.swift

---
+++ contentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  windows
+//
+//  Created by Derek Wood on 7/23/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
